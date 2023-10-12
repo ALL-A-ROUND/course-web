0fda4d3a495bef56bdd75b72137820898307fb26
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2023-10-12 08:38:16 +0800

    Added Microsoft Clarity
    Signed-off-by: Hsuan <34859514+hsuan1117@users.noreply.github.com>

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,17 +1,30 @@
 import './globals.css'
 
 import localFont from 'next/font/local'
+import Script from "next/script";
 
 const jf = localFont({src: "../fonts/jf.ttf"})
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en" className={`h-full ${jf.className}`}>
-      {/*
+export default function RootLayout({children}) {
+    return (
+        <html lang="zh" className={`h-full ${jf.className}`}>
+        <Script id="ms-clarity" strategy="afterInteractive">
+            {`(function (c, l, a, r, i, t, y) {
+                c[a] = c[a] || function () {
+                    (c[a].q = c[a].q || []).push(arguments)
+                };
+                t = l.createElement(r);
+                t.async = 1;
+                t.src = "https://www.clarity.ms/tag/" + i;
+                y = l.getElementsByTagName(r)[0];
+                y.parentNode.insertBefore(t, y);
+            })(window, document, "clarity", "script", "j95q53c3qt");`}
+        </Script>
+        {/*
         <head /> will contain the components returned by the nearest parent
         head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
-      <body className={"h-full"}>{children}</body>
-    </html>
-  )
+        <head/>
+        <body className={"h-full"}>{children}</body>
+        </html>
+    )
 }
