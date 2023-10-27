d3dde4227587f548834990f1459d614ada45298c
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2023-10-28 04:13:38 +0800

    PassKey
    Signed-off-by: Hsuan <34859514+hsuan1117@users.noreply.github.com>

--- app/auth/layout.jsx
+++ app/auth/layout.jsx
@@ -1,12 +1,14 @@
 "use client";
 import Title from "@/app/auth/Title";
 import Link from "next/link";
-import {useRouter} from "next/navigation";
+import {usePathname, useRouter} from "next/navigation";
 import {useEffect} from "react";
+import {KeyIcon} from "@heroicons/react/24/outline";
+import {api} from "@/app/utils";
 
 export default function ({children}) {
     const router = useRouter()
-
+    const pathname = usePathname()
 
     useEffect(() => {
         const token = localStorage.getItem("token")
@@ -23,6 +25,69 @@ export default function ({children}) {
             //     localStorage.removeItem("token")
         })
     })
+
+    const signInWithPasskey = () => {
+        String.prototype.toURLSafe = function () {
+            return this.replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
+        };
+
+        String.prototype.toURLUnsafe = function () {
+            let new_string = this.replace(/-/g, '+').replace(/_/g, '/');
+            while (new_string.length % 4)
+                new_string += '=';
+            return new_string;
+        };
+
+        ArrayBuffer.prototype.toBase64 = function () {
+            return btoa([].reduce.call(new Uint8Array(this), function (p, c) {
+                return p + String.fromCharCode(c)
+            }, '')).toURLSafe();
+        };
+
+        // fetch challenge
+        let rpId = process.env.NEXT_PUBLIC_PASSKEY_RPID;
+
+        api('GET', '/passkey/challenge').then(({challenge}) => {
+            if (window.PublicKeyCredential && PublicKeyCredential.isConditionalMediationAvailable) {
+                Promise.all([
+                    PublicKeyCredential.isConditionalMediationAvailable()
+                ]).then(async (results) => {
+                    if (results.every(r => r === true)) {
+                        const publicKeyCredentialRequestOptions = {
+                            challenge: Uint8Array.from(
+                                atob(challenge
+                                    .replace(/-/g, '+')
+                                    .replace(/_/g, '/')), c => c.charCodeAt(0)
+                            ),
+                            // The same RP ID as used during registration
+                            rpId,
+                        };
+
+                        const credential = await navigator.credentials.get({
+                            publicKey: publicKeyCredentialRequestOptions,
+                            mediation: 'conditional'
+                        });
+                        const response = credential.response;
+                        console.log(response);
+
+                        api("POST", "/passkey/login", {
+                            credential_id: credential.id,
+                            challenge: JSON.parse(new TextDecoder("utf-8").decode(response.clientDataJSON)).challenge,
+                            signature: response.signature.toBase64(),
+                            userHandle: response.userHandle.toBase64(),
+                            clientDataJSON: response.clientDataJSON.toBase64(),
+                            authenticatorData: response.authenticatorData.toBase64(),
+                        }).then(({token}) => {
+                            localStorage.setItem("token", token)
+                            router.push('/')
+                        }).catch(err => {
+                            console.log(err)
+                        })
+                    }
+                })
+            }
+        })
+    }
 
     return (
         <>
@@ -41,43 +106,38 @@ export default function ({children}) {
                         <div className="mt-8">
                             <div>
                                 <div>
-                                    <p className="text-sm font-medium text-gray-700">第三方登入 (尚未完成)</p>
+                                    <p className="text-sm font-medium text-gray-700">第三方登入</p>
 
                                     <div className="mt-1 grid grid-cols-3 gap-3">
-                                        <div>
-                                            <Link href="#"
-                                                  className="cursor-not-allowed bg-gray-100 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50"
+                                        {pathname === "/auth/login" && (<div>
+                                            <button onClick={signInWithPasskey}
+                                                    className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50"
                                             >
-                                                <span className="sr-only">Sign in with Facebook</span>
-                                                <svg className="h-5 w-5" aria-hidden="true" fill="currentColor"
-                                                     viewBox="0 0 20 20">
+                                                <span className="sr-only">Sign in with Passkey</span>
+
+                                                <KeyIcon className="text-gray-500 h-5 w-5"/>
+                                            </button>
+                                        </div>)}
+
+                                        <div>
+                                            <Link
+                                                href={process.env.NEXT_PUBLIC_API_ENDPOINT + "/../oauth/google"}
+                                                className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50"
+                                            >
+                                                <span className="sr-only">Sign in with Google</span>
+                                                <svg className="text-gray-500 h-5 w-5" role="img" fill="currentColor"
+                                                     viewBox="0 0 24 24"
+                                                     xmlns="http://www.w3.org/2000/svg"><title>Google</title>
                                                     <path
-                                                        fillRule="evenodd"
-                                                        d="M20 10c0-5.523-4.477-10-10-10S0 4.477 0 10c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V10h2.54V7.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V10h2.773l-.443 2.89h-2.33v6.988C16.343 19.128 20 14.991 20 10z"
-                                                        clipRule="evenodd"
-                                                    />
+                                                        d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"/>
                                                 </svg>
                                             </Link>
                                         </div>
 
                                         <div>
                                             <Link
-                                                href={"#"}
-                                                className="cursor-not-allowed bg-gray-100 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50"
-                                            >
-                                                <span className="sr-only">Sign in with Twitter</span>
-                                                <svg className="h-5 w-5" aria-hidden="true" fill="currentColor"
-                                                     viewBox="0 0 20 20">
-                                                    <path
-                                                        d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84"/>
-                                                </svg>
-                                            </Link>
-                                        </div>
-
-                                        <div>
-                                            <Link
-                                                href={process.env.NEXT_PUBLIC_API_ENDPOINT+ "/../oauth/github"}
-                                                className="bg-gray-100 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50"
+                                                href={process.env.NEXT_PUBLIC_API_ENDPOINT + "/../oauth/github"}
+                                                className=" inline-flex w-full justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50"
                                             >
                                                 <span className="sr-only">Sign in with GitHub</span>
                                                 <svg className="h-5 w-5" aria-hidden="true" fill="currentColor"
---
+++ app/member/security/page.jsx
@@ -0,0 +1,73 @@
+"use client"
+import useUser from "@/app/useUser";
+import {api} from "@/app/utils";
+import {KeyIcon} from "@heroicons/react/24/outline";
+
+export default function SecurityPage() {
+    String.prototype.toURLSafe = function () {
+        return this.replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
+    };
+
+    String.prototype.toURLUnsafe = function () {
+        let new_string = this.replace(/-/g, '+').replace(/_/g, '/');
+        while (new_string.length % 4)
+            new_string += '=';
+        return new_string;
+    };
+
+    ArrayBuffer.prototype.toBase64 = function () {
+        return btoa([].reduce.call(new Uint8Array(this), function (p, c) {
+            return p + String.fromCharCode(c)
+        }, '')).toURLSafe();
+    };
+    const user = useUser()
+    const create = async () => {
+        api('GET', '/passkey/list', {}).then((data) => {
+            console.log(user)
+            api('GET', '/passkey/challenge', {}).then(async ({challenge}) => {
+                const publicKeyCredentialCreationOptions = {
+                    challenge: Uint8Array.from(
+                        atob(challenge
+                            .replace(/-/g, '+')
+                            .replace(/_/g, '/')), c => c.charCodeAt(0)
+                    ),
+                    rp: {
+                        name: process.env.NEXT_PUBLIC_APP_NAME,
+                        id: process.env.NEXT_PUBLIC_PASSKEY_RPID,
+                    },
+                    user: {
+                        id: Uint8Array.from(user?.id, c => c.charCodeAt(0)),
+                        name: user?.name,
+                        displayName: user?.name,
+                    },
+                    pubKeyCredParams: [{alg: -7, type: "public-key"}],
+                    excludeCredentials: data.map((passkey) => ({
+                        id: Uint8Array.from(passkey.id, c => c.charCodeAt(0)),
+                        type: "public-key",
+                        transports: ["internal"],
+                    })),
+                    authenticatorSelection: {
+                        requireResidentKey: true,
+                    }
+                }
+
+                const credential = await navigator.credentials.create({
+                    publicKey: publicKeyCredentialCreationOptions
+                });
+                const response = credential.response;
+                api('POST', '/passkey/register', {
+                    credential_id: credential.id,
+                    public_key: response.getPublicKey().toBase64(),
+                    public_key_algo: response.getPublicKeyAlgorithm(),
+                    challenge: JSON.parse(new TextDecoder("utf-8").decode(response.clientDataJSON)).challenge,
+                })
+            })
+        })
+    }
+    return (
+        <div>
+            <button onClick={create} className={"flex items-center gap-2"}>Create a Passkey <KeyIcon
+                className={"h-5 w-5"}/></button>
+        </div>
+    )
+}
