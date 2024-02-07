ef31b0384d00a35cb51dc6af81347cc8693665d0
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2024-02-08 00:35:24 +0800

    add qrcode auth endpoint
    Signed-off-by: Hsuan <34859514+hsuan1117@users.noreply.github.com>

--- app/auth/page.jsx
+++ app/auth/page.jsx
@@ -228,7 +228,7 @@ export default function Auth() {
                                             <QRCode
                                                 size={256}
                                                 style={{height: "auto", maxWidth: "100%", width: "100%"}}
-                                                value={"fs"}
+                                                value={`qrauth://${process.env.NEXT_PUBLIC_API_ENDPOINT}|${qrID}`}
                                                 viewBox={`0 0 256 256`}
                                             />
                                         }
