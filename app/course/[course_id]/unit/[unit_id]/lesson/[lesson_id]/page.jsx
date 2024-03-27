f582f2ebabac932c6c5e6fbf24ab8e4fdfb3171b
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2024-03-27 14:26:59 +0800

    timer
    Signed-off-by: Hsuan <34859514+hsuan1117@users.noreply.github.com>

--- app/course/[course_id]/unit/[unit_id]/lesson/[lesson_id]/page.jsx
+++ app/course/[course_id]/unit/[unit_id]/lesson/[lesson_id]/page.jsx
@@ -6,14 +6,16 @@ import {useEffect, useState} from "react";
 import DiscussPage from "@/app/course/[course_id]/discuss/page";
 
 export default function LessonPage({params: {course_id, unit_id, lesson_id}}) {
+    const [player, setPlayer] = useState(null)
     const [watchTime, setWatchTime] = useState(null)
+    const [timer, setTimer] = useState(null)
     const {
         data: lesson,
         isLoading
     } = useSWR(`/lesson/${lesson_id}`, async (url) => await api("GET", url, null).then(d => d))
 
-    useEffect(()=>{
-        if(lesson?.video) {
+    useEffect(() => {
+        if (lesson?.video) {
             api('GET', `/lesson/${lesson_id}/watch`).then(d => {
                 setWatchTime(d.time)
             })
@@ -21,10 +23,26 @@ export default function LessonPage({params: {course_id, unit_id, lesson_id}}) {
     }, [lesson])
 
     useEffect(() => {
-        setTimeout(() => {
-            alert("10秒請移動滑鼠以確保您的觀看狀態不會被系統判定為閒置。")
-        }, 1000 * 15)
-    }, []);
+        // register mouse move event
+        window.addEventListener('mousemove', updateTimer)
+        window.addEventListener('keydown', updateTimer)
+        return () => {
+            window.removeEventListener('mousemove', updateTimer)
+            window.removeEventListener('keydown', updateTimer)
+        }
+    })
+
+    const updateTimer = async () => {
+        console.log('[INFO] Update timer')
+        clearTimeout(timer)
+        setTimer(setTimeout(alertWatch, Math.random() * 10000 + 120*1000))
+    }
+
+    const alertWatch = async () => {
+        player?.pauseVideo()
+        console.log("[INFO] User is not watching the video")
+        alert('請移動您的滑鼠或按任意鍵以保持觀看狀態')
+    }
 
     return (
         <div className="overflow-hidden bg-white sm:rounded-md">
@@ -51,14 +69,22 @@ export default function LessonPage({params: {course_id, unit_id, lesson_id}}) {
                                     time: e.target.getCurrentTime(),
                                 })
                             }}
-                            onReady={e=>{
+                            onReady={e => {
+                                setPlayer(e.target)
                                 e.target.seekTo(watchTime)
+                            }}
+                            onPlay={async e => {
+                                await api('POST', `/lesson/${lesson_id}/watch`, {
+                                    time: e.target.getCurrentTime()
+                                })
+                                await updateTimer()
                             }}
                         />
                     ) : "本堂課程未提供影片"}
 
                     {
-                        watchTime !== null && <div className={"text-gray-500"}>上次觀看到：{moment.duration(moment().add(watchTime, 'seconds').diff(moment())).humanize()}</div>
+                        watchTime !== null && <div
+                            className={"text-gray-500"}>上次觀看到：{moment.duration(moment().add(watchTime, 'seconds').diff(moment())).humanize()}</div>
                     }
 
                     <div className={"h-full my-8"}>
