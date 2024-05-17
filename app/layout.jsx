78e305018b7cd2a5a038c299bcd9b756c8bd0881
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2024-05-17 16:18:05 +0800

    Fix tracking
    Signed-off-by: Hsuan <34859514+hsuan1117@users.noreply.github.com>

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,24 +5,17 @@ import localFont from 'next/font/local'
 import Script from "next/script";
 import {useAuthState} from "react-firebase-hooks/auth";
 import {auth} from "@/lib/firebase/firebase";
+import dynamic from 'next/dynamic';
 
 const jf = localFont({src: "../fonts/jf.ttf"})
 export default function RootLayout({children}) {
     const [user, loading] = useAuthState(auth)
+    const TrackingComponent = dynamic(() => import(`@/components/tracking/${process.env.NEXT_PUBLIC_APP_ID}`), {
+        ssr: false,
+    });
     return (
         <html lang="zh" className={`h-full ${jf.className}`}>
-        <Script id="ms-clarity" strategy="afterInteractive">
-            {`(function (c, l, a, r, i, t, y) {
-                c[a] = c[a] || function () {
-                    (c[a].q = c[a].q || []).push(arguments)
-                };
-                t = l.createElement(r);
-                t.async = 1;
-                t.src = "https://www.clarity.ms/tag/" + i;
-                y = l.getElementsByTagName(r)[0];
-                y.parentNode.insertBefore(t, y);
-            })(window, document, "clarity", "script", "j95q53c3qt");`}
-        </Script>
+        <TrackingComponent/>
         {/*
         <head /> will contain the components returned by the nearest parent
         head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
@@ -31,7 +24,7 @@ export default function RootLayout({children}) {
         <body className={"h-full"}>
         {loading ? (
             <div>瘋狂載入中....</div>
-        ): children}
+        ) : children}
 
         <script data-jsd-embedded data-key="b3282d69-8c52-48af-9cf8-60e9b573fca8"
                 data-base-url="https://jsd-widget.atlassian.com"
