3216e45b89c6ec22f03d49261b1a6acaabf7567c
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2024-06-05 18:19:27 +0800

    Add side bar dropdown
    Signed-off-by: Hsuan <34859514+hsuan1117@users.noreply.github.com>

--- app/(home)/(navigation-components)/account-relation.tsx
+++ app/(home)/(navigation-components)/account-relation.tsx
@@ -6,18 +6,45 @@ import {
     DropdownMenuItem,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
-import { Menu } from "lucide-react"
+import {DoorClosed, Menu, Settings, User} from "lucide-react"
+import {useAuthState, useSignOut} from "react-firebase-hooks/auth";
+import {auth} from "@/lib/firebase/firebase";
+import {Mail} from "lucide-react"
+
+import {Button} from "@/components/ui/button"
+import Link from "next/link";
+import {signOut} from "@/lib/firebase/auth";
+
 
 export default function AccountRelation() {
+    const [user, loading, error] = useAuthState(auth)
+
+    if (loading) return <div>Loading...</div>
     return (
         <div className="size-8">
             <DropdownMenu>
                 <DropdownMenuTrigger className="">
-                    <Menu className="size-8 p-1.5 cursor-pointer hover:bg-year-100 rounded-lg" />
+                    <Menu className="size-8 p-1.5 cursor-pointer hover:bg-year-100 rounded-lg"/>
                 </DropdownMenuTrigger>
                 <DropdownMenuContent className="w-72 h-[20rem] bg-white">
-                    <DropdownMenuItem className="h-full flex flex-col justify-center">
-                        現在購物車是空的，快去選購課程吧！
+                    <DropdownMenuItem className="h-full flex flex-col items-start">
+                        {user ? (
+                            <div className={"flex flex-col"}>
+                                <Link href='/member'>
+                                    <Button>
+                                        <Settings className="mr-2 h-4 w-4"/> 設定
+                                    </Button>
+                                </Link>
+                            </div>
+                        ) : (
+                            <div className={"flex flex-col"}>
+                                <Link href='/auth'>
+                                    <Button>
+                                        <User className="mr-2 h-4 w-4"/> 登入帳號
+                                    </Button>
+                                </Link>
+                            </div>
+                        )}
                     </DropdownMenuItem>
                 </DropdownMenuContent>
             </DropdownMenu>
