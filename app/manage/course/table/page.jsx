079f1aacfda17dda744620293e5abb7e0136253e
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2024-06-12 15:18:37 +0800

    開課
    Signed-off-by: Hsuan <34859514+hsuan1117@users.noreply.github.com>

--- app/manage/course/table/page.jsx
+++ app/manage/course/table/page.jsx
@@ -2,6 +2,7 @@
 import {ChevronLeftIcon, ChevronRightIcon} from '@heroicons/react/20/solid'
 import useSWR from "swr";
 import {api} from "@/app/utils";
+import Link from "next/link";
 
 function Heading() {
     return (
@@ -42,18 +43,19 @@ function Heading() {
                     </h2>
                 </div>
                 <div className="mt-4 flex flex-shrink-0 md:ml-4 md:mt-0">
-                    <button
-                        type="button"
-                        className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                    >
-                        Edit
-                    </button>
-                    <button
+                    {/*<button*/}
+                    {/*    type="button"*/}
+                    {/*    className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"*/}
+                    {/*>*/}
+                    {/*    Edit*/}
+                    {/*</button>*/}
+                    <Link
+                        href={'/manage/course/new'}
                         type="button"
                         className="ml-3 inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                     >
-                        Publish
-                    </button>
+                        開課
+                    </Link>
                 </div>
             </div>
         </div>
@@ -98,6 +100,8 @@ export default function Page() {
                         </div>
                     ))
                 }
+
+                { courses && courses.length === 0 && '您目前沒有課程'}
 
 
             </div>
