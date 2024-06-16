19ef4079a08bc05ae460c6282c7d32e8e6f85714
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2024-06-17 04:56:38 +0800

    Facebook
    Signed-off-by: Hsuan <34859514+hsuan1117@users.noreply.github.com>

--- app/auth/page.jsx
+++ app/auth/page.jsx
@@ -7,6 +7,7 @@ import {
     signInWithCustomToken,
     signInWithPopup,
     GoogleAuthProvider,
+    FacebookAuthProvider,
     sendPasswordResetEmail,
     signInWithRedirect,
 } from "firebase/auth";
@@ -16,6 +17,7 @@ import {QrCodeIcon} from "@heroicons/react/24/solid";
 import QRCode from "react-qr-code";
 import {api} from "@/app/utils";
 import {ArrowPathIcon, ArrowPathRoundedSquareIcon, CheckCircleIcon, CheckIcon} from "@heroicons/react/24/outline";
+import {FacebookIcon} from "lucide-react";
 
 const {detect} = require('detect-browser');
 const browser = detect();
@@ -110,6 +112,13 @@ export default function Auth() {
             signInWithPopup(auth, new GoogleAuthProvider())
         }
     }
+    const facebookLogin = () => {
+        if (browser?.name === "safari") {
+            signInWithRedirect(auth, new FacebookAuthProvider())
+        } else {
+            signInWithPopup(auth, new FacebookAuthProvider())
+        }
+    }
 
     const forgotPassword = e => {
         e.preventDefault();
@@ -138,55 +147,55 @@ export default function Auth() {
                 <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
                     <div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
                         <form className="space-y-6" onSubmit={isForgot ? forgotPassword : emailLogin}>
-                            <div>
-                                <label htmlFor="role" className="block text-sm font-medium leading-6 text-gray-900">
-                                    登入權限
-                                </label>
-                                <div className="mt-2">
-                                    <fieldset aria-label="Plan">
-                                        <div className="space-y-5">
+                            {/*<div>*/}
+                            {/*    <label htmlFor="role" className="block text-sm font-medium leading-6 text-gray-900">*/}
+                            {/*        登入權限*/}
+                            {/*    </label>*/}
+                            {/*    <div className="mt-2">*/}
+                            {/*        <fieldset aria-label="Plan">*/}
+                            {/*            <div className="space-y-5">*/}
 
-                                            <div className="relative flex items-start">
-                                                <div className="flex h-6 items-center">
-                                                    <input
-                                                        id="business"
-                                                        name="plan"
-                                                        type="radio"
-                                                        className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                                                    />
-                                                </div>
-                                                <div className="ml-3 text-sm leading-6">
-                                                    <label className="font-medium text-gray-900">
-                                                        個人用戶
-                                                    </label>
-                                                    <p className="text-gray-500">
-                                                        可為學生亦可為老師
-                                                    </p>
-                                                </div>
-                                            </div>
-                                            <div className="relative flex items-start">
-                                                <div className="flex h-6 items-center">
-                                                    <input
-                                                        id="business"
-                                                        name="plan"
-                                                        type="radio"
-                                                        className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                                                    />
-                                                </div>
-                                                <div className="ml-3 text-sm leading-6">
-                                                    <label className="font-medium text-gray-900">
-                                                        企業用戶
-                                                    </label>
-                                                    <p className="text-gray-500">
-                                                        企業用戶統一管理
-                                                    </p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </fieldset>
+                            {/*                <div className="relative flex items-start">*/}
+                            {/*                    <div className="flex h-6 items-center">*/}
+                            {/*                        <input*/}
+                            {/*                            id="business"*/}
+                            {/*                            name="plan"*/}
+                            {/*                            type="radio"*/}
+                            {/*                            className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"*/}
+                            {/*                        />*/}
+                            {/*                    </div>*/}
+                            {/*                    <div className="ml-3 text-sm leading-6">*/}
+                            {/*                        <label className="font-medium text-gray-900">*/}
+                            {/*                            個人用戶*/}
+                            {/*                        </label>*/}
+                            {/*                        <p className="text-gray-500">*/}
+                            {/*                            可為學生亦可為老師*/}
+                            {/*                        </p>*/}
+                            {/*                    </div>*/}
+                            {/*                </div>*/}
+                            {/*                <div className="relative flex items-start">*/}
+                            {/*                    <div className="flex h-6 items-center">*/}
+                            {/*                        <input*/}
+                            {/*                            id="business"*/}
+                            {/*                            name="plan"*/}
+                            {/*                            type="radio"*/}
+                            {/*                            className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"*/}
+                            {/*                        />*/}
+                            {/*                    </div>*/}
+                            {/*                    <div className="ml-3 text-sm leading-6">*/}
+                            {/*                        <label className="font-medium text-gray-900">*/}
+                            {/*                            企業用戶*/}
+                            {/*                        </label>*/}
+                            {/*                        <p className="text-gray-500">*/}
+                            {/*                            企業用戶統一管理*/}
+                            {/*                        </p>*/}
+                            {/*                    </div>*/}
+                            {/*                </div>*/}
+                            {/*            </div>*/}
+                            {/*        </fieldset>*/}
 
-                                </div>
-                            </div>
+                            {/*    </div>*/}
+                            {/*</div>*/}
                             <div>
                                 <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
                                     電子郵件
@@ -281,20 +290,14 @@ export default function Auth() {
                                     <span className="text-sm font-semibold leading-6">Google</span>
                                 </button>
 
-                                <a
-                                    href="#"
+                                <button
+                                    onClick={facebookLogin}
                                     className="flex w-full items-center justify-center gap-3 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:ring-transparent"
                                 >
-                                    <svg className="h-5 w-5 fill-[#24292F]" aria-hidden="true" fill="currentColor"
-                                         viewBox="0 0 20 20">
-                                        <path
-                                            fillRule="evenodd"
-                                            d="M10 0C4.477 0 0 4.484 0 10.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0110 4.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.942.359.31.678.921.678 1.856 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0020 10.017C20 4.484 15.522 0 10 0z"
-                                            clipRule="evenodd"
-                                        />
-                                    </svg>
-                                    <span className="text-sm font-semibold leading-6">GitHub</span>
-                                </a>
+                                    <FacebookIcon className="h-5 w-5"/>
+
+                                    <span className="text-sm font-semibold leading-6">Facebook</span>
+                                </button>
 
                                 <button
                                     onClick={generateQR}
