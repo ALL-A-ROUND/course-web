3af42f86bd36958df21bc5dc0578031d436f438d
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2024-07-10 06:25:13 -0400

    update
    Signed-off-by: Hsuan <34859514+hsuan1117@users.noreply.github.com>

--- app/[lng]/(home)/(components)/founding.tsx
+++ app/[lng]/(home)/(components)/founding.tsx
@@ -11,68 +11,8 @@ import {
 import demoImage from "@/public/temp/course-view.avif"
 import { FoundingCourse } from "@/components/course_card"
 
-const carouselItems = [
-    {
-        id: 1,
-        image: demoImage,
-        title: "陳的故事行銷課 學得下去就能活得下去的本事",
-        alt: "image",
-        progress: 1003,
-        price: 1000,
-        original_price: 2000,
-        produced_by: "陳大文",
-        hot: true,
-    }, {
-        title: "陳的故事行銷課 學得下去就能活得下去的本事",
-        id: 2,
-        image: demoImage,
-        alt: "image",
-        progress: 720,
-        price: 1000,
-        original_price: 2000,
-        produced_by: "陳大文",
-        hot: true
-    }, {
-        title: "陳的故事行銷課 學得下去就能活得下去的本事",
-        id: 3,
-        image: demoImage,
-        alt: "image",
-        progress: 20,
-        price: 1000,
-        original_price: 2000,
-        produced_by: "陳大文",
-        hot: false
-    }, {
-        title: "陳的故事行銷課 學得下去就能活得下去的本事",
-        id: 4,
-        image: demoImage,
-        alt: "image",
-        progress: 325,
-        price: 1000,
-        original_price: 2000,
-        produced_by: "陳大文",
-        hot: false
-    }, {
-        title: "陳的故事行銷課 學得下去就能活得下去的本事",
-        id: 5,
-        image: demoImage,
-        alt: "image",
-        progress: 68,
-        price: 1000,
-        original_price: 2000,
-        produced_by: "陳大文",
-        hot: false
-    }, {
-        title: "陳的故事行銷課 學得下去就能活得下去的本事",
-        id: 6,
-        image: demoImage,
-        alt: "image",
-        progress: 37,
-        price: 1000,
-        original_price: 2000,
-        produced_by: "陳大文",
-        hot: false
-    },
+const carouselItems:any = [
+
 ]
 
 export default function FoundingSection() {
@@ -91,8 +31,8 @@ export default function FoundingSection() {
             >
                 <CarouselContent className="">
                     {carouselItems.map((item, cnt) => (
-                        <CarouselItem className="md:basis-1/3 basis-full select-none cursor-pointer" key={item.id}>
-                            <FoundingCourse {...item} />
+                        <CarouselItem className="md:basis-1/3 basis-full select-none cursor-pointer" key={item?.id}>
+                            {/*<FoundingCourse {...item} />*/}
                         </CarouselItem>
                     ))}
                 </CarouselContent>
